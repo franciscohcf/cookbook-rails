69337e844a03140a79a80ffb2134537a8cdbe6e5
franciscohcf <franciscocavalcantefilho@gmail.com>
2021-11-30 14:36:09 -0300

    feat: index page changed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
+  def index
+    render plain: 'página principal'
+  end
+
   def ola
     render(plain: 'Você está na página (/ola)')
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root to: 'home#index'
   get '/ola', to: 'home#ola' # visitor => site => home_controller => def ola() => response
 end
