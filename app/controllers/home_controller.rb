c846141ec455dbde6839b24b3d825da9a4c82cee
franciscohcf <franciscocavalcantefilho@gmail.com>
2021-11-30 15:12:21 -0300

    feat: added a list of recipes in index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,9 @@
 
 class HomeController < ApplicationController
   def index
+    r1 = Recipe.new(name: 'Pudim', recipe_type: 'Sobremesa')
+    r2 = Recipe.new(name: 'Bolo de cenoura', recipe_type: 'Sobremesa')
+
+    @recipes = [r1, r2]
   end
 end
---
+++ app/models/recipe.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class Recipe
+  attr_reader :name, :recipe_type
+
+  def initialize(name:, recipe_type:)
+    @name = name
+    @recipe_type = recipe_type
+  end
+end
